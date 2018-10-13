2a6e546c9edc97caea33d85c13749ff3d59211f4
Luisferdk <luisferdk@bitbucket.org>
2018-10-13 12:16:33 -0400

    index2.ph

--- public/index2.php
+++ public/index2.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../rennytravel/vendor/autoload.php';
+require __DIR__.'/../rennytours/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../rennytravel/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../rennytravel/bootstrap/app.php';
+$app = require_once __DIR__.'/../rennytours/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
