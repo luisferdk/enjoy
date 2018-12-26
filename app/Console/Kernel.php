8c74359a458cffa89391d47ab7ee67d61315933b
juanb <juanb@manzanares.com.ve>
2018-12-25 21:15:26 -0400

    fix cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\CouponController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,6 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function(){
+            $coupon = new CouponController();
+            $coupon->updateStateCoupon();
+        })->daily();
         // $schedule->command('inspire')
         //          ->hourly();
     }
