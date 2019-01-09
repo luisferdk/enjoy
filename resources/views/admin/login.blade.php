8d0b4bb70682adc32d41a30c42b956daea988df9
juanb <juanb@manzanares.com.ve>
2019-01-08 21:31:02 -0400

    nada

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -80,7 +80,7 @@
                             </div>
                         </center>
                     </form>
-                    <a href="{{ url('register') }}">Registro de agencia</a>
+                    <a href="{{ url('register') }}">Registro de agencias</a>
                 </div>
             </div>
         </center>
