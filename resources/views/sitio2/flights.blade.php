f03eb45c12be40f4dccd57f4794d2f287c4a7712
Luisferdk <luisferdk@github.com>
2019-02-11 23:32:52 -0400

    ordenando el html del formulario flight

--- resources/views/sitio2/flights.blade.php
+++ resources/views/sitio2/flights.blade.php
@@ -1,26 +1,23 @@
 @extends('layouts.sitio')
 @section('content')
-<div class="container">
-  <form action="" class="row fields" id="formFlights" method="post" ng-submit="agregarVuelo($event)">
-    <div class="col-12 mt-5">
-      <div class="card">
-        <div class="card-header m-0 p-0">
-          <h4 class="col-12 text-white text-center mt-3 mb-3">Flights</h4>
-        </div>
-        <div class="card-body">
+<div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="text-center m-0 p-0 text-white">Flights</h4>
+      </div>
+      <div class="card-body">
+        <form action="" class="fields" method="post" ng-submit="agregarVuelo($event)">
           <div class="row">
             <div class="col-4">
               <div class="form-group">
-                <select class="form-control select2" name="origen" ng-model="vuelo.origen" ng-options="aux.origen for aux in vuelos"
-                  required>
+                <select class="form-control select2" name="origen" ng-model="vuelo.origen" ng-options="aux.origen for aux in vuelos" required>
                   <option value>From</option>
                 </select>
               </div>
             </div>
             <div class="col-4">
               <div class="form-group">
-                <select class="form-control select2" name="destino" ng-model="vuelo.destino" ng-options="aux.descripcion group by aux.categoria for aux in vuelo.origen.destinos"
-                  required>
+                <select class="form-control select2" name="destino" ng-model="vuelo.destino" ng-options="aux.descripcion group by aux.categoria for aux in vuelo.origen.destinos" required>
                   <option value="">To</option>
                 </select>
               </div>
@@ -35,13 +32,14 @@
                 </select>
               </div>
             </div>
+
             <div class="col-12">
               <div class="form-group">
                 <label for="">
                   *Date Flight
                 </label>
-                <input ng-model="vuelo.fecha" class="form-control fecha" name="fecha" type="text" placeholder="Select Date"
-                  required>
+                <input ng-model="vuelo.fecha" class="form-control fecha" name="fecha"
+                  type="text" placeholder="Select Date" required>
               </div>
             </div>
             <div class="col-12 mt-2 mb-1">
@@ -50,19 +48,18 @@
               </h3>
             </div>
             <div class="col-12 text-center">
-              <button class="btn btn-primary" ng-click="opcion='agregar'" name="vuelo" type="submit" value="vuelo">
+              <button class="btn btn-primary" ng-click="opcion='agregar'" name="vuelo" type="submit"
+                value="vuelo">
                 Add to <i class="ion-ios-cart"></i>
               </button>
-              <button class="btn btn-default vuelo" ng-click="opcion='reservar'" name="vuelo" type="submit" value="vuelo">Book
-                now</button>
+              <button class="btn btn-default vuelo" ng-click="opcion='reservar'" name="vuelo" type="submit"
+                value="vuelo">Book now</button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
-  </form>
 </div>
-
 <section class="ftco-section mt-5 mb-5">
     <div class="container">
       <div class="row no-gutters">
