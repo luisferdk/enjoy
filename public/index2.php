2e929785e1e593ffc76f0e5b5c5e920d897c6f1b
Luisferdk <luisferdk@github.com>
2019-02-19 01:43:03 -0400

    config deployment

--- public/index2.php
+++ public/index2.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../das/vendor/autoload.php';
+require __DIR__.'/../enjoyment/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../das/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../das/bootstrap/app.php';
+$app = require_once __DIR__.'/../enjoyment/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
