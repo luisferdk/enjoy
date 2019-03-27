cee4bb9255184d0e575149ce08a7ea3c4acc02dd
Luisferdk <luisferdk@github.com>
2019-03-27 18:49:00 -0400

    quitando links

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,6 @@ Route::get('/ipn', 'SiteController@ipn');
 Route::get('/session', "SiteController@sessionGet");
 Route::post('/session', "SiteController@sessionPost");
 Route::get('/borrar', "SiteController@borrar");
-//Route::get('/index', "SiteController@index");
-//Route::get('/partyBoats', "SiteController@partyBoats");
-//Route::get('/partyBoats/{id}', "SiteController@tour");
-//Route::get('/tours', "SiteController@tours");
-//Route::get('/tour/{id}', "SiteController@tour");
-//Route::get('/packages', "SiteController@packages");
-//Route::post('/packages', "SiteController@packagesPOST");
-//Route::get('/wifiServices', "SiteController@wifiServices");
-//Route::get('/puntacana', "SiteController@puntacana");
-//Route::get('/shop', "SiteController@shopGet");
-
 
 
 Auth::routes();
